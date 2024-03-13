9e57f0ea94814c2b56ab7958489c47b569a3cad2
javierfonde <javifondevila01@gmail.com>
2024-03-13 01:10:06 -0300

    Proyecto Desafio Tecnico

--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -1,24 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
+
 import { TextField, Button } from "@mui/material"
 
-function TaskInput ({task, setTask, addTask}){
+import './TaskInput.scss'
+
+function TaskInput({ addTask }) {
+
+    const [task, setTask] = useState("")
+
+    const handleAddTask = (e) => {
+        e.preventDefault()
+        addTask(task)
+        setTask("");// set de cero la nueva tarea
+    }
+
     return (
-    <><div style={{ padding: "10px" }}>
-            <TextField
-                label="Nueva Tarea"
-                variant="filled"
-                style={{ width: '500px', backgroundColor: "#BDB9A9", borderRadius: "4px" }}
-                value={task}
-                onChange={event => setTask(event.target.value)}>
-            </TextField>
-        </div>
-        <Button
-            variant="contained"
-            style={{ backgroundColor: "#1e2833" }}
-            onClick={addTask}
-        >Agregar Tarea
-        </Button>
-    </>
+        <>
+            <div>
+                <TextField
+                    label="Nueva Tarea"
+                    variant="filled"
+                    className="input-adaptable"
+                    style={{ backgroundColor: "#BDB9A9", borderRadius: "4px" }}
+                    value={task}
+                    onChange={event => setTask(event.target.value)}>
+                </TextField>
+            </div>
+            <Button
+                variant="contained"
+                style={{ backgroundColor: "#1e2833", padding: '1rem', marginTop:"1rem", marginBottom:"1rem" }}
+                onClick={handleAddTask}
+                className="boton-agregar"
+            >
+                Agregar Tarea
+            </Button>
+
+
+        </>
     );
 }
 export default TaskInput;
